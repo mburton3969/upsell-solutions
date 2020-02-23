729b4a7e5bbefb1116ed1a408af8461a3d07b474
Upsell Developer <developer@upsell-solutions.com>
2020-02-22 22:26:59 -0600

    session format

--- assets/php/view-tokens.php
+++ assets/php/view-tokens.php
@@ -14,7 +14,9 @@ echo '<h4>User Token:</h4>';
 echo '<p>' . $_SESSION['user_token'] . '</p>';
 
 echo '<h4>Session Data:</h4>';
-echo '<p>' . print_r($_SESSION) . '</p>';
+echo '<p>';
+echo json_decode($_SESSION, JSON_PRETTY_PRINT);
+echo '</p>';
 
 
 ?>
