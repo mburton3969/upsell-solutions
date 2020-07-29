437691b5eaa629e9e9a8bcd6e72b1ee88f8e52ca
Upsell Developer <developer@upsell-solutions.com>
2020-07-29 11:34:15 -0500

    Cleared for CI4 Framework

--- assets/js/errors.js
+++
@@ -1,6 +0,0 @@
-function throwError(title,mess,btnt){
-  document.getElementById('eTitle').innerHTML = title;
-  document.getElementById('eMess').innerHTML = mess;
-  document.getElementById('eBtn').innerHTML = btnt;
-  $("#errorModal").modal("show");
-}
