1776cbebb4185480e0f44ea814dd1eeedd00fab4
Michael Burton <michael@ignition-innovations.com>
2020-10-05 16:22:37 -0400

    added inseam and changed DB Connection

---
+++ specifics/sections/inseam-table.php
@@ -0,0 +1,13 @@
+<div class="table-card">
+    <table class="table table-bordered table-hover table-striped" id="inseam_table">
+        <thead class="bg-primary">
+            <tr style="padding:5px;">
+                <th style="padding:5px;">Inseam List &nbsp;&nbsp;&nbsp; <input type="text" class="" id="new_12_option" placeholder="Inseam" style="color:#000;" /></th>
+                <th style="padding:5px;"><button type="button" class="btn btn-success btn-sm" onclick="add_specific(12);" style="padding: 0px 5px;">Add</button></th>
+            </tr>
+        </thead>
+        <tbody id="12_tbody">
+        
+        </tbody>
+    </table>
+</div>
